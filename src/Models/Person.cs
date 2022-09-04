4f6e4944c0da06475add25e9dd03934ed26e2fc8
Anderson Damasceno <willk@atlantico04416.virgo.atlantico.net.br>
2022-09-03 21:23:13 -0300

    First Video

--- src/Models/Person.cs
+++ src/Models/Person.cs
@@ -20,7 +20,7 @@ public class Person
         Active = true;
     }
 
-    public int Id { get; set}
+    public int Id { get; set; }
     public string? Name { get; set; }
     public int Age { get; set; }
     public string? CPF { get; set; }
